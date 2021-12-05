6fc7f93e0a60010a41a30786ed0d1b68036e35c1
Quasar-Kim <vhfflrhs128@gmail.com>
2021-12-05 09:41:32 +0900

    karma 테스트 설정

--- test/injectmap.js
+++ test/injectmap.js
@@ -1,8 +1,9 @@
-// https://generator.jspm.io/#HYwxDsIwDEU9MHCZWG1KkdhylSQYZNomVWxF6tSJe2Ox/vfe/14ArmfT7Ki8uRC8eCUnsVMLHgecgZ/JLXT0uIa7DRN8pBbX9uyMhwE9jg/YWDVMhgeoSaj1mOwmr1HElBFH2BupHi4dShJmtCsQ3nazSOM/9SA1L6TI1UqmouFmqf8BWLPkyqMA
+// https://generator.jspm.io/#HY2xDsIwDEQ9MPAzttqkRWLLr6TBgEmaVolVKVMn/puI9d69u+8F4HoWDcj5JZkx+rJ6CG8vbiJLEzwlMVZ/cHGGBppBHgtGbodP7tYDC5+6ZSx7wM7dQIbGO6yi6mzHA+yFVRsuTbm6mboDVda9r7L6f8dA3UJkJdkwJOGs/Xsk8wMvo58enQA
 
 const map = `
 {
   "imports": {
+    "chai": "https://ga.jspm.io/npm:chai@4.3.4/index.mjs",
     "file-saver": "https://ga.jspm.io/npm:file-saver@2.0.5/dist/FileSaver.min.js",
     "idb-keyval": "https://ga.jspm.io/npm:idb-keyval@6.0.3/dist/index.js",
     "json-rpc-2.0": "https://ga.jspm.io/npm:json-rpc-2.0@0.2.19/dist/dev.index.js",
@@ -13,11 +14,14 @@ const map = `
   },
   "scopes": {
     "https://ga.jspm.io/": {
+      "assertion-error": "https://ga.jspm.io/npm:assertion-error@1.1.0/index.js",
       "backo2": "https://ga.jspm.io/npm:backo2@1.0.2/index.js",
       "base64-arraybuffer": "https://ga.jspm.io/npm:base64-arraybuffer@0.1.4/lib/base64-arraybuffer.js",
-      "buffer": "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.12/nodelibs/browser/buffer.js",
+      "buffer": "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.13/nodelibs/browser/buffer.js",
+      "check-error": "https://ga.jspm.io/npm:check-error@1.0.2/index.js",
       "component-emitter": "https://ga.jspm.io/npm:component-emitter@1.3.0/index.js",
-      "debug": "https://ga.jspm.io/npm:debug@4.3.2/src/browser.js",
+      "debug": "https://ga.jspm.io/npm:debug@4.3.3/src/browser.js",
+      "deep-eql": "https://ga.jspm.io/npm:deep-eql@3.0.1/index.js",
       "engine.io-client": "https://ga.jspm.io/npm:engine.io-client@5.1.2/lib/index.js",
       "engine.io-client/contrib/xmlhttprequest-ssl/XMLHttpRequest.js": "https://ga.jspm.io/npm:engine.io-client@5.1.2/lib/xmlhttprequest.js",
       "engine.io-client/lib/globalThis.js": "https://ga.jspm.io/npm:engine.io-client@5.1.2/lib/globalThis.browser.js",
@@ -25,13 +29,16 @@ const map = `
       "engine.io-parser": "https://ga.jspm.io/npm:engine.io-parser@4.0.3/lib/index.js",
       "engine.io-parser/lib/decodePacket.js": "https://ga.jspm.io/npm:engine.io-parser@4.0.3/lib/decodePacket.browser.js",
       "engine.io-parser/lib/encodePacket.js": "https://ga.jspm.io/npm:engine.io-parser@4.0.3/lib/encodePacket.browser.js",
+      "get-func-name": "https://ga.jspm.io/npm:get-func-name@2.0.0/index.js",
       "has-cors": "https://ga.jspm.io/npm:has-cors@1.1.0/index.js",
       "ms": "https://ga.jspm.io/npm:ms@2.1.2/index.js",
       "parseqs": "https://ga.jspm.io/npm:parseqs@0.0.6/index.js",
       "parseuri": "https://ga.jspm.io/npm:parseuri@0.0.6/index.js",
-      "process": "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.12/nodelibs/browser/process.js",
+      "pathval": "https://ga.jspm.io/npm:pathval@1.1.1/index.js",
+      "process": "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.13/nodelibs/browser/process.js",
       "safari-14-idb-fix": "https://ga.jspm.io/npm:safari-14-idb-fix@3.0.0/dist/index.js",
       "socket.io-parser": "https://ga.jspm.io/npm:socket.io-parser@4.0.3/dist/index.js",
+      "type-detect": "https://ga.jspm.io/npm:type-detect@4.0.8/type-detect.js",
       "yeast": "https://ga.jspm.io/npm:yeast@0.1.2/index.js"
     }
   }
@@ -41,5 +48,4 @@ const map = `
 const mapElem = document.createElement('script')
 mapElem.setAttribute('type', 'importmap')
 mapElem.innerHTML = map
-document.body.append(mapElem)
-
+document.head.append(mapElem)
---
+++ test/test.test.js
@@ -0,0 +1,6 @@
+describe('test of test', () => {
+  it('should work', () => {
+    const sans = 'WA!'
+    expect(sans).to.equal('WA!')
+  })
+})
